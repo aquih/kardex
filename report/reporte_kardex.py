5a1ce0d58f4ffc3cb23bec3c8973180be0b6ee60
rocketgithub <rodrigo@solucionesprisma.com>
2021-04-30 11:08:01 -0600

    Lineas duplicadas por error

--- report/reporte_kardex.py
+++ report/reporte_kardex.py
@@ -90,8 +90,6 @@ class ReporteKardex(models.AbstractModel):
     
     @api.model
     def _get_report_values(self, docids, data=None):
-    @api.model
-    def _get_report_values(self, docids, data=None):
         model = self.env.context.get('active_model')
         docs = self.env[model].browse(self.env.context.get('active_ids', []))
 
