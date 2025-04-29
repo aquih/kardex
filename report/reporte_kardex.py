f52521e29e81f833f7db1b0940de965e96ba72d0
rocketgithub <rodrigo@aquih.com>
2025-04-29 09:34:36 -0600

    Función correcta para determinar grupos de usuario

--- report/reporte_kardex.py
+++ report/reporte_kardex.py
@@ -80,7 +80,7 @@ class ReporteKardex(models.AbstractModel):
             detalle['costo'] = 0
             detalle['total'] = 0
 
-            if self.user_has_groups('sales_team.group_sale_manager,account.group_account_user'):
+            if self.env.user.has_group('sales_team.group_sale_manager,account.group_account_user'):
                 grupos = self.env['stock.valuation.layer'].read_group([('product_id', '=', m.product_id.id), ('create_date', '<=', m.date+datetime.timedelta(seconds=2))], ['value:sum', 'quantity:sum'], ['product_id'])
                 for grupo in grupos:
                     if (grupo['quantity'] != 0):
@@ -102,6 +102,6 @@ class ReporteKardex(models.AbstractModel):
             'data': data['form'],
             'docs': docs,
             'lineas': self.lineas,
-            'mostrar_costo': self.user_has_groups('stock.group_stock_manager,sales_team.group_sale_manager,account.group_account_user'),
+            'mostrar_costo': self.env.user.has_group('stock.group_stock_manager,sales_team.group_sale_manager,account.group_account_user'),
         }
 
